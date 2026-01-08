8a7248af3a09a39e061e8ee30f47f675cae36d12
Tryston <trystonproffit@gmail.com>
2026-01-08 11:05:02 -0500

    Finished Project

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 //! Start by creating the variables for the data recorded
 //* Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
 
+const day1TempF = 32;
+const day2TempC = 25;
+const day3TempF = 70;
+const day4TempC = 18;
+const day5TempF = 80;
+const day6TempC = 15;
+const day7TempF = 72;
+const day8TempC = 28;
+const day9TempF = 68;
+const day10TempC = 20;
+const day11TempF = 75;
+const day12TempC = 23;
+const day13TempF = 82;
+const day14TempC = 30;
+const day15TempF = 65;
+const day16TempC = 22;
+const day17TempF = 77;
+const day18TempC = 26;
+const day19TempF = 78;
+const day20TempC = 24;
+const day21TempF = 73;
+const day22TempC = 21;
+const day23TempF = 79;
+const day24TempC = 27;
+const day25TempF = 71;
+const day26TempC = 19;
+const day27TempF = 74;
+const day28TempC = 17;
+const day29TempF = 76;
+const day30TempC = 29;
 
 //! Start the calculation of the total temperatures
 //* Then apply the conversion to calculate the total in the other unit of measurement
 //* Call the variables: tot_temperature_in_fahrenheit and tot_temperature_in_celsius
 
+const tot_temperature_in_fahrenheit = day1TempF + day3TempF + day5TempF + day7TempF + day9TempF + day11TempF + day13TempF + day15TempF + day17TempF + day19TempF + day21TempF + day23TempF + day25TempF + day27TempF + day29TempF + (day2TempC * 9 / 5) + 32 + (day4TempC * 9 / 5) + 32 + (day6TempC * 9 / 5) + 32 + (day8TempC * 9 / 5) + 32 + (day10TempC * 9 / 5) + 32 + (day12TempC * 9 / 5) + 32 + (day14TempC * 9 / 5) + 32 + (day16TempC * 9 / 5) + 32 + (day18TempC * 9 / 5) + 32 + (day20TempC * 9 / 5) + 32 + (day22TempC * 9 / 5) + 32 + (day24TempC * 9 / 5) + 32 + (day26TempC * 9 / 5) + 32 + (day28TempC * 9 / 5) + 32 + (day30TempC * 9 / 5) + 32;
+
+const tot_temperature_in_celsius = (tot_temperature_in_fahrenheit - 32) * 5 / 9;
+
+console.log(tot_temperature_in_fahrenheit);
+console.log(tot_temperature_in_celsius);
+
 //! Start the calculation of the average temperatures
 //* Call the variables: avg_temperature_in_fahrenheit and avg_temperature_in_celsius
+
+const avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit / 30;
+const avg_temperature_in_celsius = tot_temperature_in_celsius / 30;
+
+console.log(avg_temperature_in_fahrenheit);
+console.log(avg_temperature_in_celsius);
 
 //! Console.log the results for your own inspection if you'd like
 
@@ -15,8 +58,4 @@
 //* This way you can export them to the test file, this is essential for the tests to work
 
 module.exports = {
-    // tot_temperature_in_fahrenheit,
-    // tot_temperature_in_celsius,
-    // avg_temperature_in_fahrenheit,
-    // avg_temperature_in_celsius
 };
